0ec6667294c0a99a8197eb53639ee4ee41922863
Laysla Guedes <130912085+layslaguedes@users.noreply.github.com>
2023-05-08 17:07:18 -0300

    Add files via upload

---
+++ exe1.java
@@ -0,0 +1,33 @@
+/* Somar 4 números e mostrar o resultado
+ * Laysla Anizio Guedes
+ * 03/05/2023
+ */
+
+import java.util.Scanner;
+
+public class exe1{
+    public static void main (String[] args){
+        Scanner ex = new Scanner(System.in);
+
+        // Declaração e Inicialização das variaveis
+
+        System.out.println("Digite um valor: ");
+        int n1 = ex.nextInt();
+        System.out.println("Digite um valor: ");
+        int n2 = ex.nextInt();
+        System.out.println("Digite um valor: ");
+        int n3 = ex.nextInt(); 
+        System.out.println("Digite um valor: ");
+        int n4 = ex.nextInt();
+
+        // Calculo com as variaveis
+
+        int res = n1+n2+n3+n4;
+
+        // Resultado
+
+        System.out.println("O resultado é: " + res);
+
+        ex.close();
+    }   
+ } 
