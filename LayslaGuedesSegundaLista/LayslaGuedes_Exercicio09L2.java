f19fdf029f44fefd1feeb174d8cf49f971e60669
Laysla Guedes <130912085+layslaguedes@users.noreply.github.com>
2023-05-21 20:55:16 -0300

    LayslaGuedes_Exercicio09L2.java

--- LayslaGuedesSegundaLista/LayslaGuedes_Exercicio09L2.java
+++ LayslaGuedesSegundaLista/LayslaGuedes_Exercicio09L2.java
@@ -5,20 +5,25 @@ package exercícioL2;
  * Data: 17/05/2023
  */
 
+ //importação da class Scanner
  import java.util.Scanner;
 
+//inicialização da class principal
 public class LayslaGuedes_Exercicio09L2 {
     public static void main(String[] args){
         Scanner exe9 = new Scanner(System.in);
 
+        //criação e inicialização das variavais
         double n1, n2;
         n1 = 0.0;
         n2 = 0.0;
     
+        // recebendo e lendo as variaveis
         System.out.println(" Digite dois valores: ");
         n1 = exe9.nextInt();
         n2 = exe9.nextInt();
         
+        //montando a logica e dando o resultado
         if(n1 == n2){
             System.out.println(" Os números são iguais: ");
 
