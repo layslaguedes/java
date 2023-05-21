ec69ba941355056762df96a07f8df1076e0c0acd
Laysla Guedes <130912085+layslaguedes@users.noreply.github.com>
2023-05-21 20:39:03 -0300

    LayslaGuedes_Exercicio04L2.java

--- LayslaGuedesSegundaLista/LayslaGuedes_Exercicio04L2.java
+++ LayslaGuedesSegundaLista/LayslaGuedes_Exercicio04L2.java
@@ -5,21 +5,26 @@ package exercícioL2;
  * Data: 17/05/2023
  */
 
+//importação da class Scanner
 import java.util.Scanner;
 
+//inicialização da class principal
 public class LayslaGuedes_Exercicio04L2 {
     public static void main(String[] args){
         Scanner exe4 = new Scanner(System.in);
 
+        //criação e inicialização das variaveis
         int b, a, c;
         b = 0;
         a = 0;
         c = 0;
 
+        //recebendo e lendo os valores
         System.out.println(" Digite dois números: ");
         b = exe4.nextInt();
         a = exe4.nextInt();
 
+        //montando a logica e mostrando o resultado
         c = a;
         a = b;
         b = c;
