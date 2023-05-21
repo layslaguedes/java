f60823a5d7b29648564837e40a06e4508627bb06
Laysla Guedes <130912085+layslaguedes@users.noreply.github.com>
2023-05-21 20:31:13 -0300

    LayslaGuedes_Exercicio02L2.java

--- LayslaGuedesSegundaLista/LayslaGuedes_Exercicio02L2.java
+++ LayslaGuedesSegundaLista/LayslaGuedes_Exercicio02L2.java
@@ -4,23 +4,27 @@ package exercícioL2;
  * Data: 17/05/2023
 */
 
-
+// importação da class Scanner
 import java.util.Scanner;
 
+// inicialização da class principal
 public class LayslaGuedes_Exercicio02L2 {
     public static void main(String[] args){
         Scanner exe2 = new Scanner(System.in);
 
+        // criação e inicialização das variaveis
         int n1, n2, n3;
         n1 = 0;
         n2 = 0;
         n3 = 0;
 
+        // recebendo e lendo os valores
         System.out.println(" Digite 3 números: ");
         n1= exe2.nextInt();
         n2= exe2.nextInt();
         n3= exe2.nextInt();
-
+    
+        // montando a logica e mostrando o resultado
         if (n1 > n2 && n1 > n3){
             System.out.println(n1 + " é o maior número ");
         } else if (n2 > n1 && n2 > n3){
