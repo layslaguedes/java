da4c5a403fd83e1e459a2ab2703d85e6d67b450f
Laysla Guedes <130912085+layslaguedes@users.noreply.github.com>
2023-05-21 20:51:21 -0300

    LayslaGuedes_Exercicio07L2.java

--- LayslaGuedesSegundaLista/LayslaGuedes_Exercicio07L2.java
+++ LayslaGuedesSegundaLista/LayslaGuedes_Exercicio07L2.java
@@ -5,18 +5,23 @@ package exercícioL2;
  * Data: 17/05/2023
  */
 
+//importação da class Scanner
  import java.util.Scanner;
 
+//inicialização da class principal
 public class LayslaGuedes_Exercicio07L2 {
     public static void main(String[] args){
         Scanner exe7 = new Scanner(System.in);
 
+        //criação e inicialização das variaveis
         int n1;
         n1 = 0;
     
+        //recebendo e lendo os valores
         System.out.println(" Digite um valor: ");
         n1 = exe7.nextInt();
         
+        //montando a logica e mostrando o resultado
         if(n1>94 && n1 <456){
             System.out.println(" Você acertou: ");
 
