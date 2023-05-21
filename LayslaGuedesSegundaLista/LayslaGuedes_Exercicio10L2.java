8e75f46879c1e59c116563ae16e825e068e1e524
Laysla Guedes <130912085+layslaguedes@users.noreply.github.com>
2023-05-21 20:59:05 -0300

    LayslaGuedes_Exercicio10L2.java

--- LayslaGuedesSegundaLista/LayslaGuedes_Exercicio10L2.java
+++ LayslaGuedesSegundaLista/LayslaGuedes_Exercicio10L2.java
@@ -5,18 +5,23 @@ package exercícioL2;
  * Data: 17/05/2023
  */
 
+//importação da class Scanner
  import java.util.Scanner;
 
+//inicialização da class principal
 public class LayslaGuedes_Exercicio10L2 {
      public static void main(String[] args){
          Scanner exe10 = new Scanner(System.in);
  
+         //criação e inicialização das variavais
          int n1;
          n1 = 0;
      
+         // recebendo e lendo as variaveis
          System.out.println(" Digite um valor: ");
          n1 = exe10.nextInt();
          
+         //montando a logica e mostrando o resultado
          if((n1%2)==0){
              System.out.println("número par");
          } else {
